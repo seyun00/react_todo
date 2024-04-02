f0a459d64a8e2a62574c9740216229cca948e06b
seyun00 <reny83@naver.com>
2024-04-02 11:13:33 +0900

    3rd commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -7,6 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// mongoose.connect('mongodb://localhost:27017/todolist')
 mongoose.connect(process.env.DB_ATLAS_URL)
 
 app.get('/get',async(req,res)=>{
